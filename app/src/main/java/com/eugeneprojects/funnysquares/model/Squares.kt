3555e112181bbe0625b1f5d8b7e726bb6c16baa9
EugeneGridnev <eugenegrid@yandex.ru>
2024-02-22 20:35:35 +0300

    made Squares parcelable

--- app/src/main/java/com/eugeneprojects/funnysquares/model/Squares.kt
+++ app/src/main/java/com/eugeneprojects/funnysquares/model/Squares.kt
@@ -1,9 +1,13 @@
 package com.eugeneprojects.funnysquares.model
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 class Squares private constructor(
     val size: Int,
     val colors: List<Int>
-) {
+) :Parcelable {
   constructor(
       size: Int,
       colorProducer: () -> Int
--- app/src/main/java/com/eugeneprojects/funnysquares/ui/MainViewModel.kt
+++ app/src/main/java/com/eugeneprojects/funnysquares/ui/MainViewModel.kt
@@ -1,17 +1,22 @@
 package com.eugeneprojects.funnysquares.ui
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.eugeneprojects.funnysquares.model.Squares
 import kotlin.random.Random
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    savedStateHandle: SavedStateHandle
+) : ViewModel() {
 
-    private val _squares = MutableLiveData<Squares>()
+    private val _squares = savedStateHandle.getLiveData<Squares>(KEY_SQUARES)
     val squares: LiveData<Squares> = _squares
 
     init {
+        if (!savedStateHandle.contains(KEY_SQUARES)) {
+            savedStateHandle[KEY_SQUARES] = createSquares()
+        }
         generateSquares()
     }
 
@@ -24,5 +29,9 @@ class MainViewModel : ViewModel() {
             size = Random.nextInt(5, 11),
             colorProducer = { -Random.nextInt(0xFFFFFF) }
         )
+    }
+
+    companion object {
+        const val KEY_SQUARES = "squares"
     }
 }
