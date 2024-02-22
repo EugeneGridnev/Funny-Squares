22b22c1750eb7691108335d28118bdee2e94e4f1
EugeneGridnev <eugenegrid@yandex.ru>
2024-02-22 20:43:49 +0300

    viewmodel factory added

--- app/src/main/java/com/eugeneprojects/funnysquares/ui/MainActivity.kt
+++ app/src/main/java/com/eugeneprojects/funnysquares/ui/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel by viewModels<MainViewModel>()
+    private val viewModel by viewModels<MainViewModel> { ViewModelFactory(this) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
---
+++ app/src/main/java/com/eugeneprojects/funnysquares/ui/ViewModelFactory.kt
@@ -0,0 +1,18 @@
+package com.eugeneprojects.funnysquares.ui
+
+import androidx.lifecycle.AbstractSavedStateViewModelFactory
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.savedstate.SavedStateRegistryOwner
+
+class ViewModelFactory(
+    owner: SavedStateRegistryOwner
+) : AbstractSavedStateViewModelFactory(owner, null) {
+    override fun <T : ViewModel> create(
+        key: String,
+        modelClass: Class<T>,
+        handle: SavedStateHandle
+    ): T {
+        return MainViewModel(handle) as T
+    }
+}
